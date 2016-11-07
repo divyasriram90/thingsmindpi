2d3c57606d56fab0189f82f5f7179aa561b6cac8
divya-sourcebits <divsriram90@gmail.com>
2016-11-07 09:37:44 +0530

    changed pin in led

--- listen.py
+++ listen.py
@@ -7,7 +7,7 @@ pubnub = Pubnub(
     publish_key = "pub-c-5b4ed4d3-8921-45cb-8151-d48aaebb2467",
     subscribe_key = "sub-c-f7b16a98-a291-11e6-a1b1-0619f8945a4f")
 
-led = LED(20)
+led = LED(17)
 channel = "manage"
 
 def callback(message, channel):
